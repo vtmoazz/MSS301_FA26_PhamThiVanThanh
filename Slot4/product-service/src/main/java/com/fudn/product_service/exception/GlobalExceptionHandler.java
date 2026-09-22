77f3ecdf060ba5a4743de519c30d6d6727f3bff4
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-22 08:50:57 +0700

    feat(exception): add GlobalExceptionHandler returning 404 (TODO 5)

---
+++ Slot4/product-service/src/main/java/com/fudn/product_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.fudn.product_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(ProductNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
