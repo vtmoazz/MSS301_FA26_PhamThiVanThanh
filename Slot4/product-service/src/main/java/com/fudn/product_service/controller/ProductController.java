53af1ca365754f0d3006defdfb5f2561df951606
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-22 08:50:50 +0700

    feat(delete): add DELETE /api/products/{id} endpoint (TODO 4)

--- Slot4/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ Slot4/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -74,4 +74,9 @@ public class ProductController {
     //   public void deleteProduct(@PathVariable String id) { ... }
     // ==========================================================
     // TODO: viết endpoint delete tại đây
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
 }
