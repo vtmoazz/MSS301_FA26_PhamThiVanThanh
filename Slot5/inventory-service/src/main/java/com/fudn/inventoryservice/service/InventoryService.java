b402abb61cb6f54079cd05adc1847e4630841361
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-25 13:20:13 +0700

    feat(service): xây dựng InventoryService kiểm tra tồn kho

---
+++ Slot5/inventory-service/src/main/java/com/fudn/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.fudn.inventoryservice.service;
+
+import com.fudn.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
