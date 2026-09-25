d93e07ce83598c04bd48dff6c3025c03ccea36ec
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-25 13:08:26 +0700

    feat(service): xây dựng OrderService xử lý placeOrder

---
+++ Slot5/order-service/src/main/java/com/fudn/orderservice/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.fudn.orderservice.service;
+
+import com.fudn.orderservice.dto.OrderRequest;
+import com.fudn.orderservice.model.Order;
+import com.fudn.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        var order = mapToOrder(orderRequest);
+        orderRepository.save(order);
+    }
+
+    private static Order mapToOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+        order.setSkuCode(orderRequest.skuCode());
+        return order;
+    }
+}
