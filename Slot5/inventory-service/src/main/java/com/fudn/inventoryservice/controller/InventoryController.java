c8ccd3686e55f55a9ccc1943e1e4f3007a3b9a0a
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-25 13:20:13 +0700

    feat(controller): thêm endpoint GET /api/inventory

---
+++ Slot5/inventory-service/src/main/java/com/fudn/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,23 @@
+package com.fudn.inventoryservice.controller;
+
+import com.fudn.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(
+            @RequestParam String skuCode,
+            @RequestParam Integer quantity
+    ) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
