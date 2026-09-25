74c46f93d1038b7902cb683b595be1b314e9a740
Thanh Pham Thi Van <vaanthanh2005@gmail.com>
2026-09-25 13:08:26 +0700

    feat(controller): thêm endpoint POST /api/order

---
+++ Slot5/order-service/src/main/java/com/fudn/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.fudn.orderservice.controller;
+
+import com.fudn.orderservice.dto.OrderRequest;
+import com.fudn.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
